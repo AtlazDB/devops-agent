181746450b8c655c739cc5a36f576828635cb687
Leandro Campos <leandro.campos@ebinterchange.com.br>
2026-08-17 20:08:12 -0300

    first commit

---
+++ src/main/java/br/com/fatec/devopsagent/DevopsagentApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.devopsagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsagentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatec/devopsagent/DevopsagentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.devopsagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsagentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
